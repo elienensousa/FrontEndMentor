f34806d47b01cfbd62e0a402796561c08d737082
Eliene Sousa <eliene.nunes1@hotmail.com>
2022-05-04 02:15:39 -0300

    Challenge faq accordion card

---
+++ faq-accordion-card-main/scripts/script.js
@@ -0,0 +1,15 @@
+const questions = document.querySelectorAll('.head-question')
+
+
+questions.forEach((question) => {
+    question.addEventListener('click', () =>{
+        if(question.parentNode.classList.contains('active')){
+            question.parentNode.classList.toggle('active')
+        }
+        else{
+            questions.forEach((question) => question.parentNode.classList.remove('active'))
+            question.parentNode.classList.add('active')
+        }
+    })
+});
+
